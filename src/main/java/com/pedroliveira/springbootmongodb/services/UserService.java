7f44eae9989b875ebe20d04badbe0c82b3c592c0
Pedro Oliveira <pedro86gugalegal@gmail.com>
2022-11-14 07:17:04 -0300

    Inserção de usuário com POST

--- src/main/java/com/pedroliveira/springbootmongodb/services/UserService.java
+++ src/main/java/com/pedroliveira/springbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pedroliveira.springbootmongodb.domain.User;
+import com.pedroliveira.springbootmongodb.dto.UserDTO;
 import com.pedroliveira.springbootmongodb.repository.UserRepository;
 import com.pedroliveira.springbootmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
