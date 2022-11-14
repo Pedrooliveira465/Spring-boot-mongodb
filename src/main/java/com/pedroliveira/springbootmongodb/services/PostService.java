fe48e4d6ce3889915825bd299b460e82a996b2af
Pedro Oliveira <pedro86gugalegal@gmail.com>
2022-11-14 08:59:32 -0300

    Obtendo um post por id

---
+++ src/main/java/com/pedroliveira/springbootmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.pedroliveira.springbootmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedroliveira.springbootmongodb.domain.Post;
+import com.pedroliveira.springbootmongodb.repository.PostRepository;
+import com.pedroliveira.springbootmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
